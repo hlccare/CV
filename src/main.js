99cff6565d3def9cbb576eee7ff0dd34099251e1
hlccare <280108904@qq.com>
2021-01-26 20:00:08 +0800

    八卦

--- src/main.js
+++ src/main.js
@@ -1,27 +1,72 @@
-let demo = document.querySelector('#demo')
+let html = document.querySelector('#html')
+let style = document.querySelector('#style')
 let string = `
-你好，我是一名前端新人
-接下来加样式
-body{
-    color: red;
-}`
+/* 你好，我是一名前端新人
+ * 接下来演示一下前端
+ * 首先我要准备一个div
+ */
+#div1{
+    border: 1px solid red;
+    width: 200px;
+    height: 200px;
+}
+/* 接下来我把div变成一个八卦图
+ * 首先将div变成一个圆
+*/
+#div1{
+    border-radius: 50%;
+    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    border: none;
+}
+/* 八卦是阴阳形成的
+ * 一黑一白
+ */
+#div1{
+    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
+}
+/* 图形调整
+ */
+#div1::before{
+    width: 100px;
+    height: 100px;
+    left: 50%;
+    top: 0;
+    transform: translateX(-50%);
+    background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);
+    border-radius: 50%;
+}
+#div1::after{
+    width: 100px;
+    height: 100px;
+    left: 50%;
+    top: 50%;
+    transform: translateX(-50%);
+    background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%);
+    border-radius: 50%;
+}
+`
 // string = string.replace(/\n/g, '<br>')
 let string2 = ''
 let n = 0
 
 let step = () => {
     setTimeout(() => {
-        if(string[n] === '\n'){
-            string2 += '<br>'
-        }else{
+        if (string[n] === '\n') {
+            string2 += '<br>';
+        } else if (string[n] === ' ') {
+            string2 += '&nbsp'
+        } else {
             string2 += string[n];
         }
-        demo.innerHTML = string2
-        if(n < string.length-1){
+        html.innerHTML = string2
+        style.innerHTML = string.substring(0, n)
+        if (n < string.length - 1) {
             n += 1;
             step();
+            window.scrollTo(0, 9999)
+            html.scrollTo(0, 9999)
         }
-    }, 100);
+    }, 50);
 }
 
 step();
@@ -29,3 +74,11 @@ step();
 //     n = n + 1
 //     demo.innerHTML = n;
 // }, 1000);
+
+// setTimeout(() => {
+//     style.innerHTML = `
+//     body{
+//         color: red;
+//     }
+//     `
+// }, 3000)
